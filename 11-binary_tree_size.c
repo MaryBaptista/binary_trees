b11fc781bf80b75c5001f19825e32417540f03a0
MaryBaptista <marybaptistanwachukwu@gmail.com>
2022-09-24 13:33:30 -0700

    Task 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,40 +1,32 @@
 #include "binary_trees.h"
 
 /**
- * leaf_counter - Counts the number of leaves in a binary tree.
+ * node_counter - Computes the number of nodes in a binary tree.
  * @tree: The binary tree.
- * @n: A pointer to the tree's leaf count value.
+ * @n: A pointer to the tree's node count value.
  */
 
-void leaf_counter(const binary_tree_t *tree, size_t *n)
+void node_counter(const binary_tree_t *tree, size_t *n)
 {
 	if (tree != NULL)
 	{
-		if ((tree->left == NULL) && (tree->right == NULL))
-		{
-			if (n != NULL)
-			{
-				(*n)++;
-			}
-		}
-		else
-		{
-			leaf_counter(tree->left, n);
-			leaf_counter(tree->right, n);
-		}
+		if (n != NULL)
+			(*n)++;
+		node_counter(tree->left, n);
+		node_counter(tree->right, n);
 	}
 }
 
 /**
- * binary_tree_leaves - Counts the number of leaves in a binary tree.
+ * binary_tree_size - Computes the number of nodes in a binary tree.
  * @tree: The binary tree.
- * Return: The number of leaves in the given binary tree.
+ * Return: The number of nodes in the given binary tree.
  */
 
-size_t binary_tree_leaves(const binary_tree_t *tree)
+size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t n = 0;
 
-	leaf_counter(tree, &n);
+	node_counter(tree, &n);
 	return (n);
 }
